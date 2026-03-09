91ea0020fd208839546824d25e0f045bb7572eb0
delyamino <panda.mulad@gmail.com>
2026-03-09 11:14:26 +0300

    Созданы классы People, Person и main, дополнен pom файл

---
+++ Maven1/src/main/java/com/mephi/maven1/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mephi.maven1;
+
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import org.yaml.snakeyaml.Yaml;
+/**
+ *
+ * @author panda
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.setOut(new PrintStream(System.out, true, "UTF-8"));
+        } catch (UnsupportedEncodingException ex) {
+            System.getLogger(Main.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+        Yaml yaml = new Yaml();
+        InputStream inputStream = People.class.getClassLoader().getResourceAsStream("yamlPeople.yaml");
+        People data = yaml.loadAs(inputStream, People.class);
+        List<Person> people = data.getPeople();
+        
+        for (Person person : people) {
+            System.out.println("Имя:"+person.getName()+"  "+"Адрес:"+person.getAdress());
+            System.out.println();
+        }
+    }
+}
---
+++ Maven1/src/main/java/com/mephi/maven1/People.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mephi.maven1;
+
+import java.util.List;
+
+/**
+ *
+ * @author panda
+ */
+public class People {
+    private List<Person> people;
+    
+    public List<Person> getPeople() {
+        return people;
+    }
+    
+    public void setPeople(List<Person> people) {
+        this.people = people;
+    }
+}
